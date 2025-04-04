65938eb4ab221f6c7fa954b73505b5867379a1f2
Prabhat1615 <prabhatkumar958873@gmail.com>
2025-04-05 02:51:14 +0530

    added signup page

--- lib/Screens/Auth/Signup.dart
+++ lib/Screens/Auth/Signup.dart
@@ -1,15 +1,217 @@
+import 'package:dialup/main.dart';
 import 'package:flutter/material.dart';
 
-class Signup extends StatelessWidget {
+class Signup extends StatefulWidget {
   const Signup({super.key});
 
   @override
+  State<Signup> createState() => _SignupState();
+}
+
+class _SignupState extends State<Signup> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Signup'),
+      backgroundColor: Colors.black,
+      body: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 50, horizontal: 20),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text('Create an account✨',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  )),
+              SizedBox(height: 10),
+              const Text(
+                'Welcome! Please enter your details.',
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.grey,
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10, left: 10),
+                child: Text("Name",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+              Padding(
+                padding: EdgeInsets.only(left: 0, right: 0, top: 0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      prefixIcon: Icon(Icons.person),
+                      hintText: 'Enter Your Name',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6))),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10, left: 10),
+                child: Text("Email",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 0, right: 0, top: 0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.mail),
+                      hintText: 'Enter your email',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6))),
+                ),
+              ),
+              const Padding(
+                padding: EdgeInsets.only(top: 10, left: 10),
+                child: Text("Password",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    )),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 0, right: 0, top: 0),
+                child: TextFormField(
+                  decoration: InputDecoration(
+                      prefixIcon: const Icon(Icons.password),
+                      hintText: 'Password',
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(6))),
+                ),
+              ),
+              Row(
+                children: [
+                  Checkbox(
+                    value: false,
+                    onChanged: (value) {},
+                    checkColor: Colors.white,
+                    activeColor: Colors.blue,
+                  ),
+                  const Text(
+                    "Must be at least 8 characters",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
+              ),
+              MaterialButton(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6),
+                ),
+                color: Colors.deepOrange,
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Signup(),
+                      ));
+                },
+                child: Container(
+                  width: double.infinity,
+                  child: const Text(
+                    'Sign Up',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              MaterialButton(
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(6),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Signup(),
+                      ));
+                },
+                child: Container(
+                  width: double.infinity,
+                  child: const Row(
+                    children: [
+                      Icon(Icons.g_mobiledata, color: Colors.white, size: 30),
+                      Text(
+                        'Sign Up with Goggle',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              MaterialButton(
+                shape: RoundedRectangleBorder(
+                  side: const BorderSide(color: Colors.grey),
+                  borderRadius: BorderRadius.circular(6),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => Signup(),
+                      ));
+                },
+                child: Container(
+                  width: double.infinity,
+                  child: const Row(
+                    children: [
+                      Icon(Icons.facebook, color: Colors.white, size: 30),
+                      Text(
+                        'Sign Up with Facebook',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              const Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: const Center(
+                  child: Text(
+                    'Already have an account?Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Text('Signup Page'),
     );
   }
 }
