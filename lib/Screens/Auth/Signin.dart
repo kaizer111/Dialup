f95e7330cef06140975cb2f593acc7d3d4c7bb53
kaizer111 <mahatovishal2001@gmail.com>
2025-04-04 14:39:05 +0530

    added two screens

---
+++ lib/Screens/Auth/Signin.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Signin extends StatelessWidget {
+  const Signin({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Signup'),
+      ),
+      body: Text('Signup Page'),
+    );
+  }
+}
